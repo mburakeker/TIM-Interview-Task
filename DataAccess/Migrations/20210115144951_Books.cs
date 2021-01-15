8ba1c6c92fa052b81830e4e691f6f2e7d6d9febc
Burak Eker <mehmetburakeker@gmail.com>
2021-01-15 18:10:11 +0300

    Added DataAccess and Entity layer
    Removed unimplemented layers back, 
    Exposed Docker WebApi and MSSQL server ports, 
    Added DB Migrations, 
    Added Book entity

---
+++ DataAccess/Migrations/20210115144951_Books.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class Books : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BookName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsbnId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CurrentHolder = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "BookName", "CurrentHolder", "IsbnId" },
+                values: new object[,]
+                {
+                    { 1, "readymade", "american", "street", "mlkshk" },
+                    { 2, "fixie", "whatever", "haven't", "fixie" },
+                    { 3, "loko", "Lo-fi", "occupy", "they" },
+                    { 4, "rights", "raw", "part", "art" },
+                    { 5, "richardson", "apparel", "beer", "narwhal" },
+                    { 6, "narwhal", "Iphone", "kogi", "freegan" },
+                    { 7, "pickled", "company", "Polaroid", "DIY" },
+                    { 8, "keffiyeh", "Ethnic", "shorts", "trust" },
+                    { 9, "bicycle", "vice", "park", "ethnic" },
+                    { 10, "Post-ironic", "post-ironic", "pickled", "Marfa" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
