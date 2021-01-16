a8081401c086a900ee4c0e6e0a18dd33ca739b2f
Burak Eker <mehmetburakeker@gmail.com>
2021-01-16 23:54:29 +0300

    Added Kibana, Elastic APM and Serilog

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -6,13 +6,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 namespace WebApi
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile(
+                    $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
+                    optional: true)
+                .Build();
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
+                {
+                    AutoRegisterTemplate = true,
+                    IndexFormat = $"{configuration["ElasticConfiguration:IndexName"]}-{DateTime.UtcNow:yyyy-MM}"
+                })
+                .Enrich.WithProperty("Environment", environment)
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -21,6 +39,7 @@ namespace WebApi
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .UseSerilog();
     }
 }
