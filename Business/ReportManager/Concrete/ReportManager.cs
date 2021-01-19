0b19509595bbae777b45037910f3f0d8f650711a
Burak Eker <mehmetburakeker@gmail.com>
2021-01-19 20:58:10 +0300

    Added Daily Report Manager/Controller

---
+++ Business/ReportManager/Concrete/ReportManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Dto;
+using Business.ReportManager.Abstract;
+using DataAccess.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Business.ReportManager.Concrete
+{
+    public class ReportManager: IReportManager
+    {
+        private readonly ILogger<ReportManager> _logger;
+        private readonly LibraryContext _context;
+        public ReportManager(ILogger<ReportManager> logger, LibraryContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        internal bool IsDatetimePastOrComingSoon(DateTime endDate,int soonAsDays)
+        {
+            return (endDate - DateTime.Now).Days <= soonAsDays;
+        }
+        public async Task<List<BookTransactionDailyReportDto>> GetDailyReport(int upcomingDays)
+        {
+            var today = DateTime.Today;
+            var activeTransactions = await _context.BookTransactions
+                .Include(bt => bt.Book)
+                .Include(bt => bt.Member)
+                .Where(bt => bt.IsActive)
+                .ToListAsync();
+            var filteredTransactions = activeTransactions
+                .Where(bt=> IsDatetimePastOrComingSoon(bt.EndDate,7))
+                .Select(bt=>new BookTransactionDailyReportDto()
+                {
+                    BookName = bt.Book.BookName,
+                    EndDate = bt.EndDate,
+                    DaysOverdue = (today - bt.EndDate).Days
+                }).ToList();
+
+            return filteredTransactions;
+        }
+    }
+}
---
+++ WebApi/Controllers/ReportController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Dto;
+using Business.ReportManager.Abstract;
+using Mapster;
+using Microsoft.Extensions.Logging;
+using WebApi.Infrastructure.Response;
+
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        private readonly ILogger<ReportController> _logger;
+        private readonly IReportManager _reportManager;
+        public ReportController(ILogger<ReportController> logger, IReportManager reportManager)
+        {
+            _logger = logger;
+            _reportManager = reportManager;
+        }
+        // GET: api/<ReportController>
+        [HttpGet]
+        public async Task<Response<BookTransactionDailyReportDto>> GetSuggestedReturnDate()
+        {
+            var response = new Response<BookTransactionDailyReportDto>();
+            try
+            {
+                response.DataList = await _reportManager.GetDailyReport(7);
+            }
+            catch (Exception ex)
+            {
+                response.ErrorList.Add(ex.Adapt<ApiException>());
+            }
+            return response;
+        }
+    }
+}
